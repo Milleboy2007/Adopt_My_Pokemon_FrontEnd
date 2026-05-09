d12692f8f6aeab6bf4898d04f34eea19b9b2906b
Milleboy2007 <Nathan2007@videotron.ca>
2026-05-09 14:33:48 -0400

    color black -> white, when hover

--- FE-Pokemon/src/components/catalogue/Card.jsx
+++ FE-Pokemon/src/components/catalogue/Card.jsx
@@ -8,9 +8,8 @@ function Card({props}) {
           <Link to={`/catalogue/${props.id}`} className='card-link'> 
             <img className='card-image' src={props.img} alt={props.nom}/>
             <p>{props.nom.charAt(0).toUpperCase() + props.nom.slice(1)}</p>
-            {/*<button onClick={() => console.log("Click on", props.nom)}>Adopté</button>*/}
-            </Link>
-            <Link to={`/adoption/${props.id}`}>Adopté</Link>
+          </Link>
+          <Link to={`/adoption/${props.id}`}>Adopté</Link>
         </li>
     </>
   )
