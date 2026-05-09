3ddecf8bdb9727b9bea49e4dfd5cdf4e9b466eda
lesvia galvez <2494155@cegepmv.ca>
2026-05-09 14:26:44 -0400

    Ajout d'un pop up dans satisfaction, verfication de css et ajout tu lien dans compte

--- FE-Pokemon/src/layouts/ProfileLayout/ProfileLayout.jsx
+++ FE-Pokemon/src/layouts/ProfileLayout/ProfileLayout.jsx
@@ -1,13 +1,14 @@
 import { Outlet, useOutletContext } from 'react-router-dom'
 import './ProfileLayout.css'
 import HeaderUser from '../../components/General/HeaderUser'
+import Footer from '../../components/General/Footer'
 
 function ProfileLayout(){
     const { user } = useOutletContext();
     let pokemons = user?.pokemons ?? [];
     
     return(
-        <div>
+        <div className='profil-layout'> 
             <HeaderUser user={user}/>
             <main>
                 <Outlet context={{user, pokemons}}/>
